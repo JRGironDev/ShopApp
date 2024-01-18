e792837df6a2d76b4e301b4f798af682f6d5368b
José Quiñónez <joserobertoq@outlook.com>
2024-01-17 20:18:52 -0600

    First Commit

---
+++ ShopApp/DataAccess/ShopDbContex.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ShopApp.DataAccess;
+
+public class ShopDbContex : DbContext
+{
+    public DbSet<Category> Categories { get; set; }
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Client> Clients { get; set; }
+}
+
+public record Category(int Id, string Nombre);
+public record Product(int Id, string Nombre, string Descripcion, decimal Precio, int CategoryId)
+{
+    public Category Category { get; set; }
+}
+public record Client(int Id, string Nombre, string Direccion);
+
+
+
