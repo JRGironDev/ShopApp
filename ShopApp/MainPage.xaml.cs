ada11d976d19170cd1740d1cd94aaba415db7f3f
José Quiñónez <joserobertoq@outlook.com>
2024-02-04 05:15:57 -0600

    Controles MAUI

--- ShopApp/MainPage.xaml.cs
+++ ShopApp/MainPage.xaml.cs
@@ -7,11 +7,6 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
-
-		var dbContext = new ShopDbContex();
-		category.Text = dbContext.Categories.Count().ToString();
-		client.Text = dbContext.Clients.Count().ToString();
-		product.Text = dbContext.Products.Count().ToString();
 	}
 }
 
